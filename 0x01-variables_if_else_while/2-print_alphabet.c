36c502138c58bdc8e3c9714c55059eb979934966
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-20 01:23:32 -0800

    Lowercase Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program prints alphabet in lower case
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+	{
+		putchar("%c", c);
+			c++;
+	}
+	putchar("\n");
+
+	return (0);
+}
