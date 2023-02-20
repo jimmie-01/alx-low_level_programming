2a1c5f0284a2d27260487e7e6a0eeecf79a1bca3
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-20 03:11:56 -0800

    Base 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - This program prints all single digit of base 10 from 0
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while
+		(c < 10)
+		{
+			putchar(c + '0');
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
+
