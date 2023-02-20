acf8e4c1f28b8fa211e55b13dfcff1b98de9f8a7
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-20 02:57:38 -0800

    Single Digit of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program prints all single digits if base ten from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
