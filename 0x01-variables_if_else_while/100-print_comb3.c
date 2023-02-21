b301043f70fb2a0418db288f5ca5386d9ea85955
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-20 18:59:14 -0800

    Possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Print all possible combinations of two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n, m;
+
+	for
+		(n = 48;
+		 n <= 58;
+		 n++)
+	{
+		for
+			(m = 49;
+			 m <= 57;
+			 m++)
+		{
+			if
+				(m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if
+					(n != 56 || m != 57)
+				{
+					putchar('.');
+					putchar(' ');
+				}
+			}
+		}
+	putchar('\n');
+	return (0);
+}
