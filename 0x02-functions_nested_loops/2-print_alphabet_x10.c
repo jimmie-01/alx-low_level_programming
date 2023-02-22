25009e0283afde53023d0ebdcdc99f16f42bd957
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-21 22:46:10 -0800

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet x10
+* Return: void
+* Description: Print lowercase alphabet x10.
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while
+		(i <= 10)
+		{
+			for
+				(c = 'a';
+				 c <= 'z';
+				 c++)
+				{
+					_putchar(c);
+				}
+			_putchar('\n');
+			i++;
+		}
+}
+
