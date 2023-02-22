1823832e6495f6245c9dc762fe9a9ccefed3ed53
Fakile Razaq <razaq_jimmie@yahoo.com>
2023-02-21 17:53:48 -0800

    Alphabet Game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Print alphabet in lowercase followed by new line.
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for
+		(c = 'a';
+		 c <= 'z';
+		 c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+}
