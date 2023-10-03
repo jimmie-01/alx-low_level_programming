b699ef48dd265734ac6965d5f5bce8ffe8fdb2f7
“Fakile <razaqjimmie91@gmail.com>
2023-10-03 08:43:52 -0700

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,7 +34,7 @@ void close_file(int fd)
 	c = close(fd);
 	if (c == -1)
 	{
-		fprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
