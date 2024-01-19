ba4c1be715c9fe024ba718c9169ae0d8bd5129c8
Fakile Razaq <razaqjimmie91@gmail.com>
2024-01-19 01:03:09 -0800

    Third Task

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/**
+ * add - add two integers
+ * @a: fisrt int
+ * @b: second int
+ * Return: a+b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - subtracts two integers
+ * @a: fisrt int
+ * @b: second int
+ * Return: a-b
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mod - find modulus of two integers
+ * @a: fisrt int
+ * @b: second int
+ * Return: a%b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: fisrt int
+ * @b: second int
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two integers
+ * @a: fisrt int
+ * @b: second int
+ * Return: a/b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
